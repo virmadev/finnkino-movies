95d705d07a3a72c1b87ca4fb60d3fd9cfc8585cd
jvirma <jorbeful@gmail.com>
2020-05-26 09:13:51 +0300

    Updates

---
+++ precache-manifest.6d544d6117514419e81578635672af6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c74068ac2ec21a205e2d1e8efe76f4",
+    "url": "/finnkino-movies/index.html"
+  },
+  {
+    "revision": "9566cc1c0466c79eda3e",
+    "url": "/finnkino-movies/static/css/main.e1e683d8.chunk.css"
+  },
+  {
+    "revision": "72544b088774087bdc0a",
+    "url": "/finnkino-movies/static/js/2.6143c536.chunk.js"
+  },
+  {
+    "revision": "27ca4020711bd21574faae3da7dd14e9",
+    "url": "/finnkino-movies/static/js/2.6143c536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9566cc1c0466c79eda3e",
+    "url": "/finnkino-movies/static/js/main.9550b1d6.chunk.js"
+  },
+  {
+    "revision": "3b405f7b204819673c10",
+    "url": "/finnkino-movies/static/js/runtime-main.7b3b67b7.js"
+  }
+]);
